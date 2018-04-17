07df3c5ff964765295096cbb7b68acea716fc8c7
Felype Gustavo <felype@ourboxdigital.com>
2018-04-17 07:25:55 -0300

    alterando o metodo de submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,48 +11,50 @@ export default class Login extends Component {
         };
     }
 
+    async postLogin() {
+        try {
+
+            const requestInfo = {
+                method: 'POST',
+                body: JSON.stringify({ login: this.login.value, senha: this.senha.value }),
+                headers: new Headers({
+                    'Content-type': 'application/json'
+                })
+            }
+
+            const response = await fetch('http://localhost:8080/api/public/login', requestInfo);
+            const responseJson = response.json;
+            localStorage.setItem('auth-token', responseJson);
+            this.setState({ logged: true })
+
+        } catch (error) {
+            this.setState({ error })
+        }
+    }
+
     doLogin = (event) => {
         event.preventDefault();
 
         this.setState({ error: '' });
+        postLogin();
+    };
 
-        const requestInfo = {
-            method: 'POST',
-            body: JSON.stringify({ login: this.login.value, senha: this.senha.value }),
-            headers: new Headers({
-                'Content-type': 'application/json'
-            })
-        }
 
-        fetch('http://localhost:8080/api/public/login', requestInfo)
-            .then(resp => {
-                if (resp.ok)
-                    return resp.text();
-                else
-                    throw new Error('Não foi possível efetuar login!');
-            })
-            .then(token => {
-                localStorage.setItem('auth-token', token);
-                this.setState({ logged: true })
-            })
-            .catch(err => this.setState({ error: err.message }));
+render() {
+    if (this.state.logged) {
+        return <Redirect to='/timeline' />
     }
 
-    render() {
-        if (this.state.logged) {
-            return <Redirect to='/timeline' />
-        }
-
-        return (
-            <div className="login-box">
-                <h1 className="header-logo">Instalura</h1>
-                <form onSubmit={this.doLogin}>
-                    <input type="text" ref={(input) => this.login = input} />
-                    <input type="password" ref={(input) => this.senha = input} />
-                    <input type="submit" value="Login" />
-                </form>
-                <span>{this.state.error}</span>
-            </div>
-        );
-    }
+    return (
+        <div className="login-box">
+            <h1 className="header-logo">Instalura</h1>
+            <form onSubmit={this.doLogin}>
+                <input type="text" ref={(input) => this.login = input} />
+                <input type="password" ref={(input) => this.senha = input} />
+                <input type="submit" value="Login" />
+            </form>
+            <span>{this.state.error}</span>
+        </div>
+    );
+}
 }
